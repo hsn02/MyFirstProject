c33f4c54ff768fcf57151f2dd6d53c55c1bf5471
Akhrafli <rafliabd017@gmail.com>
2020-02-14 02:45:23 +0700

    tambah activity register m

--- app/src/main/java/com/example/myfirstproject/Register.java
+++ app/src/main/java/com/example/myfirstproject/Register.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Register extends AppCompatActivity {
-
+  int r = 24;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
