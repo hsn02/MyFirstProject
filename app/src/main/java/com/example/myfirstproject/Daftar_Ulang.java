d904fe396a823e0259ae7e4df77051d9a5fc7604
AhmadHusain <achmadhusain211@gmail.com>
2020-02-14 03:21:17 -0800

    7890

--- app/src/main/java/com/example/myfirstproject/Daftar_Ulang.java
+++ app/src/main/java/com/example/myfirstproject/Daftar_Ulang.java
@@ -10,5 +10,7 @@ public class Daftar_Ulang extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar__ulang);
+
+        int a = 10;
     }
 }
