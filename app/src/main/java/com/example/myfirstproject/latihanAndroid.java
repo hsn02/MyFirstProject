da1167d4e859e7ce288736903fa8cfcc67f94cc4
AhmadHusain <achmadhusain211@gmail.com>
2020-02-14 01:51:52 -0800

    latihan 1

---
+++ app/src/main/java/com/example/myfirstproject/latihanAndroid.java
@@ -0,0 +1,14 @@
+package com.example.myfirstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class latihanAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latihan_android);
+    }
+}
